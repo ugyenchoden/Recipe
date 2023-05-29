dbd7d30cd6438c6d9dbb39ac5e1bdfe4f9bd5785
ugyenchoden <ugyen.choden@selise.ch>
2023-05-29 18:07:56 +0600

    delete unrequired

--- app/worker/chef_fetcher_worker.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class ChefFetcherWorker < ApplicationWorker
-  queue_as 'chef_fetcher'
-
-  def perform
-    Recipes::ChefsFetcher.new.call
-  end
-end
--- app/worker/tag_fetcher_worker.rb
+++
@@ -1,9 +0,0 @@
-# frozen_string_literal: true
-
-class TagFetcherWorker < ApplicationWorker
-  queue_as 'tag_fetcher'
-
-  def perform
-    Recipes::TagsFetcher.new.run
-  end
-end
